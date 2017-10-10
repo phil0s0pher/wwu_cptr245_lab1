6163ba569763940411150bdcd1f5f86ffa3e4c4e
phil0s0pher <c.lambert1213@gmail.com>
2017-10-09 18:39:17 -0700

    Completed dayOfTheWeek

--- lab1.cpp
+++ lab1.cpp
@@ -29,6 +29,12 @@ struct Imaginary_exception : public exception {
   }
 };
 
+struct OutOfRange_exception : public exception {
+  const char * what () const throw () {
+    return "One of the inputs is out of the acceptable range";
+  }
+};
+
 
 // Factorial
 unsigned int factorial( unsigned int number ) {
@@ -95,10 +101,102 @@ double squareRoot(double value){
   return r;
 };
 
+int getMonthValue(int month, bool leapYear) {
+  switch(month) {
+  case 1:
+    if(leapYear) {
+      return 6;
+        }
+    else {
+      return 0;
+    }
+    break;
+  case 2:
+    if(leapYear){
+      return 2;
+    }
+    else {
+      return 3;
+    }
+    break;
+  case 3:
+    return 3;
+    break;
+  case 4:
+    return 6;
+    break;
+  case 5:
+    return 1;
+    break;
+  case 6:
+    return 4;
+    break;
+  case 7:
+    return 6;
+    break;
+  case 8:
+    return 2;
+    break;
+  case 9:
+    return 5;
+    break;
+  case 10:
+    return 0;
+    break;
+  case 11:
+    return 3;
+    break;
+  case 12:
+    return 5;
+    break;
+  }
+
+  return 0;
+}
+
+bool getLeapYear(int year) {
+  return (year % 400 == 0 || (year % 4 == 0 && year % 100 > 0));
+}
+
+int getCenturyValue(int year) {
+  return (3 - ((year / 100) % 4)) * 2;
+}
+
+int getYearValue(int year) {
+  return (year % 100) + ((year % 100) / 4);
+}
+
+bool dayAllowed(int day, int month, bool leapYear) {
+  if(leapYear && day > 29 && month == 2) {
+    return false;
+  }
+
+  int dayMonthIndex[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  if(leapYear) {dayMonthIndex[1] = 29;}
+
+
+  if(day > dayMonthIndex[month - 1]) {
+    return false;
+  }
+
+  return true;
+}
 
 // Calculate what day of the week corresponds to the date.
 // Absolute C++ Ch3 PP12
-string dayOfTheWeek(int month, int day, int year);
+string dayOfTheWeek(int month, int day, int year) {
+  if(year < 1 || month < 1 || day < 1 || month > 12  || !dayAllowed(day, month, getLeapYear(year))) {
+    throw OutOfRange_exception();
+  }
+
+  bool leapYear = getLeapYear(year);
+  int centuryValue = getCenturyValue(year);
+  int yearValue = getYearValue(year);
+  int monthValue = getMonthValue(month, leapYear);
+  string dayIndex[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+  return dayIndex[(day + monthValue + yearValue + centuryValue) % 7];
+};
 
 
 // Find the student's Frist and Last Name and calculate the CS username
@@ -149,4 +247,47 @@ TEST_CASE( "Should return zero if given zero", "[squareRoot]") {
 
 TEST_CASE( "Should calculate square root of a positive double", "[squareRoot]") {
   REQUIRE( squareRoot(5) == Approx(2.2361));
+}
+
+TEST_CASE( "Should calculate leapYear", "[dayOfTheWeek]") {
+  REQUIRE( getLeapYear(2017) == false);
+  REQUIRE( getLeapYear(2016));
+}
+
+TEST_CASE( "Should calculate CenturyValue", "[dayOfTheWeek]") {
+  REQUIRE( getCenturyValue(2016) == 6);
+}
+
+TEST_CASE( "Should calculate MonthValue", "[dayOfTheWeek]") {
+  REQUIRE( getMonthValue(2, true) == 2);
+  REQUIRE( getMonthValue(2, false) == 3);
+  REQUIRE( getMonthValue(5, false) == 1);
+}
+
+TEST_CASE( "Should check if the day is allowed in that month", "[dayOfTheWeek]") {
+  REQUIRE( dayAllowed(31, 5, true));
+  REQUIRE( dayAllowed(30, 4, false));
+  REQUIRE( dayAllowed(29, 2, true));
+  REQUIRE( dayAllowed(28, 2, false));
+}
+TEST_CASE( "It should return OutOfRange_exception if year is less than 1", "[dayOfTheWeek]") {
+  REQUIRE_THROWS_AS(dayOfTheWeek(12, 5, -1000), OutOfRange_exception e);
+}
+
+TEST_CASE( "It should return OutOfRange_exception if month is less than 1", "[dayOfTheWeek]") {
+  REQUIRE_THROWS_AS(dayOfTheWeek(-5, 5, 1000), OutOfRange_exception e);
+}
+
+TEST_CASE( "It should return OutOfRange_exception if day is less than 1", "[dayOfTheWeek]") {
+  REQUIRE_THROWS_AS(dayOfTheWeek(12, 0, 1000), OutOfRange_exception e);
+}
+
+TEST_CASE( "It should return an OutOfRange_exception if month is greater than 12", "[dayOfTheWeek]") {
+  REQUIRE_THROWS_AS( dayOfTheWeek(13, 1, 2008), OutOfRange_exception e);
+}
+TEST_CASE( "It should calculate the day of the week given the day, month and year", "[dayOfTheWeek]") {
+  REQUIRE( dayOfTheWeek(10, 9, 2017) == "Monday");
+}
+TEST_CASE( "It should return an OutOfRange_exception if the day does not exist in that month", "[dayOfTheWeek]") {
+  REQUIRE_THROWS_AS( dayOfTheWeek(10, 32, 2017), OutOfRange_exception e);
 }
